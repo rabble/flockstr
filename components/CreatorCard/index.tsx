a0c8ac3e1119da87d499a7e2c2ba0167e82a6d68
rabble <evan@protest.net>
2024-08-30 19:21:43 +0200

    fixing small bugs

--- components/CreatorCard/index.tsx
+++ components/CreatorCard/index.tsx
@@ -49,18 +49,22 @@ export default function CreatorCard({
               {profile?.about}
             </CardDescription>
           </CardHeader>
-          <Image
-            alt="user"
-            src={
-              profile?.image ??
-              profile?.picture ??
-              `https://bitcoinfaces.xyz/api/get-image?name=${npub}&onchain=false`
-            }
-            className="absolute left-1/2 top-1/2 aspect-square -translate-x-1/2 -translate-y-[70%] transform overflow-hidden rounded-lg bg-muted object-cover transition-all duration-300 group-hover:left-[50px] group-hover:top-[65px] group-hover:w-[70px]"
-            height={100}
-            width={100}
-            unoptimized
-          />
+          {
+            (profile?.image || profile?.picture) && (
+              <Image
+                alt="user"
+                src={
+                  typeof profile?.image === 'string' ? profile.image :
+                  typeof profile?.picture === 'string' ? profile.picture :
+                  `https://bitcoinfaces.xyz/api/get-image?name=${npub}&onchain=false`
+                }
+                className="absolute left-1/2 top-1/2 aspect-square -translate-x-1/2 -translate-y-[70%] transform overflow-hidden rounded-lg bg-muted object-cover transition-all duration-300 group-hover:left-[50px] group-hover:top-[65px] group-hover:w-[70px]"
+                height={100}
+                width={100}
+                unoptimized
+              />
+            )
+          }
           <Card className="absolute top-full min-h-full w-5/6 overflow-hidden transition-all duration-300 group-hover:top-1/3">
             <CardHeader className="border-b p-4 pb-3">
               <CardTitle>Recent work:</CardTitle>
